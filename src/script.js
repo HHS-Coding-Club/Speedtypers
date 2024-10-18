26b58392d84dc9b600d30cd459a3eef6c62ac21a
LuckyEcho <144569592+LuckyDevelopment@users.noreply.github.com>
2024-10-18 16:20:33 -0700

    Added main files

--- src/script.js
+++ src/script.js
@@ -1,27 +1,11 @@
 // Oct, 16, 2024 - Evan & Rishi. MIT License. Simplified code and comments added to make it easier to read for beginners, although this already is pretty easy.
 
-// CONSTANTS
-const quotes = [
-    "The sunflower blossomed brightly in the summer garden.",
-    "Lemonade is the perfect drink for a sunny day at the beach.",
-    "The sun shines brightly in the clear blue sky.",
-    "Farmers work hard to harvest fresh produce for their community.",
-    "The cat chased the mouse around the house.",
-    "The sky was painted in shades of pink and orange as the sun set behind the mountains."];
+// Imports
+import { data } from "./files/data.js";
 
-const customWords = [
-    "the", "and", "a", "key", "jump", "over", "fast", "new", "left", "start", "bring", 
-    "school", "watch", "close", "down", "find", "time", "home", "early", "water", "money", 
-    "work", "right", "good", "world", "open", "play", "back", "next", "help", "small", 
-    "big", "long", "read", "write", "send", "stop", "call", "phone", "game", "store", 
-    "food", "day", "week", "month", "year", "night", "city", "drive", "park", "move", 
-    "clean", "test", "room", "line", "wait", "start", "finish", "talk", "speak", "walk", 
-    "run", "jump", "high", "low", "easy", "hard", "light", "dark", "wind", "road", 
-    "clear", "story", "word", "sound", "song", "part", "list", "group", "class", "rest", 
-    "fun", "fast", "slow", "look", "see", "meet", "learn", "grow", "plant", "green", 
-    "blue", "black", "white", "red", "stop", "turn", "bring", "set", "buy", "sell", 
-    "ride", "train", "bus", "car", "is", "on", "be", "but", "he", "she", "then", "last",
-    ];
+// CONSTANTS
+const quotes = data.quotes;
+const customWords = data.customWords;
 const keybinds = {
     "enter": function()
     {
