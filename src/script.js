79dfcfd8cc6e5ed6e9919fcc2ef4b6bf32724dd3
LuckyEcho <144569592+LuckyDevelopment@users.noreply.github.com>
2024-10-18 21:27:46 -0700

    Update V.1.0
    Added data saving, and also added a settings men. Also added a time mode where you can change how long each test takes. Also added new data file and added new quotes and words to it too.

--- src/script.js
+++ src/script.js
@@ -6,6 +6,7 @@ import { data } from "./files/data.js";
 // CONSTANTS
 const quotes = data.quotes;
 const customWords = data.customWords;
+const DATA_KEY = "test4";
 const keybinds = {
     "enter": function()
     {
@@ -24,6 +25,8 @@ let timerText = $("#timer");
 let rawWpmText = $("#rawwpm");
 let correctCharacterText = $("#correctCharacters");
 let wpmText = $("#wpm");
+let timeLeft = $("#timeLeft");
+let settingsButton = document.getElementById("settings");
 
 // DYNAMIC VARIABLES
 let currentCharIndex = 0;
@@ -34,20 +37,57 @@ let currentQuote = "";
 let timeStart = 0;
 let timeEnd = 0;
 let stillShowData = false;
+let menuOpen = false;
+let timeTestLength = 0;
+let currentTimeTest = 0;
 
 // Modes - RandomQuote
 let mode = "RandomWords";
 let totalWords = 15;
+let timerInterval = null;
+let hasStartedTimer = false;
+
+// Base Data
+let gameData = {
+    testType: "RandomWords",
+    testSeconds: 15,
+    testWords: 15,
+};
+let userData;
+
 
 // FUNCTIONS
 function Start()
 {
     // Hide the menu.
     $(".menu").hide();
+
+    // Try to get data.
+    userData = localStorage.getItem(DATA_KEY);
+    if (!userData)
+    {
+        SaveData();
+    }
+    else
+    {
+        // Parse the data.
+        userData = JSON.parse(userData);
+    }
+
+    console.log(userData);
+    
     
     // New paragraph.
     NewParagraph();
 }
+
+function SaveData()
+{
+    // Set base data.
+    localStorage.setItem(DATA_KEY, JSON.stringify(gameData));
+    userData = gameData;
+}
+
 
 function NewParagraph()
 {
@@ -57,6 +97,18 @@ function NewParagraph()
     misses = 0;
     timeStart = 0;
     timeEnd = 0;
+
+    // Set type.
+    mode = userData.testType;
+    timeTestLength = userData.testSeconds;
+    totalWords = userData.testWords;
+
+    // End timer
+    if (hasStartedTimer == true)
+    {
+        clearInterval(timerInterval);
+    }
+    hasStartedTimer = false;
     
     // Fade in and out objects
     $(".menu").fadeOut();
@@ -65,9 +117,31 @@ function NewParagraph()
     // Set random quote.
     currentQuote = ReturnRandomQuote();
     paragraphText.innerHTML = currentQuote;
+
+    // Scroll to element.
+    paragraphText.scrollIntoView();
+
+    // Start scroll
+    if (mode == "TimeTest")
+    {
+        // Set text.
+        timeLeft.text(timeTestLength + " s");
+
+        // Show time test
+        timeLeft.fadeIn();
+    }
+    else
+    {
+        timeLeft.hide();
+    }
     
     // Get ready for typing.
     isTyping = true;
+
+    // Get rid of menus.
+    $("#settingsMenu").fadeOut();
+    $("#buttonsList").fadeOut();
+    menuOpen = false;
 }
 
 function scale (number, inMin, inMax, outMin, outMax) {
@@ -76,7 +150,9 @@ function scale (number, inMin, inMax, outMin, outMax) {
 
 function ReturnRandomQuote()
 {
-    if (mode == "RandomQuote")
+   
+    // Find the mode.
+    if (mode == "RandomQuotes")
     {
         return quotes[Math.floor(Math.random() * quotes.length)];
     }
@@ -97,14 +173,65 @@ function ReturnRandomQuote()
             quote += customWords[Math.floor(Math.random() * customWords.length)] + addedSpace;
         }
         return quote;
+    } else if (mode == "TimeTest")
+    {
+        // Get random quote.
+        let quote = "";
+        let lastItem = null;
+        for (let i = 0; i < (timeTestLength * 3 + 210); i++)
+        {
+            // Add space at the end.
+            let addedSpace = "";
+            if (i != (timeTestLength * 3 + 210)- 1)
+            {
+                addedSpace = " ";
+            }
+            let chosenWord;
+
+            // Add random quote.
+            if (lastItem != null)
+            {
+                let found = false;
+                
+
+                // While random is false.
+                while (found == false)
+                {
+                    // Get rnadom word.
+                    chosenWord = customWords[Math.floor(Math.random() * customWords.length)];
+
+                    // If the last one was not this one.
+                    if (lastItem != chosenWord)
+                    {
+                        // Let it through.
+                        lastItem = chosenWord;
+                        found = true;
+                    }
+                }
+            }
+            else
+            {
+                // Get random word.
+                chosenWord = customWords[Math.floor(Math.random() * customWords.length)];
+            }
+
+            // Add it on.
+            quote += chosenWord + addedSpace;
+        }
+        return quote;
     }
 }
 
 function LoadMenu(showData)
 {
+    // Turn off stuff.
+    isTyping = false;
+    
     // Fade in and out objects
     $(".menu").fadeIn();
     $(".paragraphBox").fadeOut();
+    $("#buttonsList").fadeIn();
+    timeLeft.fadeOut();
     
     
     // Set data text.
@@ -126,49 +253,109 @@ function GetTotalWords()
 
 function GetCorrectWords()
 {
-    // Get word array. Current quote is the quote we need.
-    let words = currentQuote.split(" ");
+    let words = currentQuote.split(" "); // Split the quote into words
+    let incorrectWords = []; // Initialize an array to store incorrect words
 
-    
-   // Initialize an array to store incorrect words.
-    let incorrectWords = [];
-    
+
     // Loop through savedCharIndexs to find incorrect characters
     for (let i = 0; i < savedCharIndexs.length; i++) {
+        // If the character is incorrect
         if (!savedCharIndexs[i]) {
-            // Make variables.
-            let charIndex = 0;
-            let wordIndex = 0;
-    
-            // Find the index of the character in the words array
+            let charIndex = 0; // Index to track character position
+            let wordIndex = 0; // Index of the current word
+        
+            // Find the word that contains the incorrect character
             for (let j = 0; j < words.length; j++) {
-                if (charIndex + words[j].length + (j > 0 ? 1 : 0) > i) {
+                // Update charIndex to include the length of the current word and space
+                charIndex += words[j].length + (j > 0 ? 1 : 0); // +1 for the space
+
+                // If the charIndex exceeds the current index, this is the incorrect word
+                if (charIndex > i) {
                     wordIndex = j;
+                
                     break;
                 }
-                charIndex += words[j].length + (j > 0 ? 1 : 0); // +1 for the spaces in the string.
             }
-    
+
             // Add the incorrect word to the array if it's not already included
             if (!incorrectWords.includes(words[wordIndex])) {
                 incorrectWords.push(words[wordIndex]);
+            } else {
+                console.log("Word already exists in incorrectWords array");
             }
         }
     }
-    
+
+    // Calculate the total number of correct words
     let totalWordsGot = words.length - incorrectWords.length;
+
+    // Debugging output
+    console.log(`Total Words: ${words.length}, Incorrect Words: ${incorrectWords.length}, Total Correct Words: ${totalWordsGot}`);
+
+    // Return the total number of correct words
     return totalWordsGot;
+}
+
+function GetWordsTyped()
+{
+    let words = currentQuote.split(" "); // Split the quote into words
+    let totalCorrectWords = 0; // Initialize count of correct words
+    let currentIndex = 0; // Track the index of characters processed
+
+    // Loop through each word
+    for (let i = 0; i < words.length; i++) {
+        let word = words[i];
+        let isWordCorrect = true; // Assume the word is correct unless proven otherwise
+
+        // Check each character of the word
+        for (let charIndex = 0; charIndex < word.length; charIndex++) {
+            // Check the global index for savedCharIndexs
+            if (currentIndex < savedCharIndexs.length) {
+                if (savedCharIndexs[currentIndex] !== true) {
+                    isWordCorrect = false; // If any character is incorrect, mark the word as incorrect
+                    break; // No need to check further characters
+                }
+            } else {
+                // If we run out of saved indices, we consider it incorrect
+                isWordCorrect = false;
+                break;
+            }
+            currentIndex++; // Move to the next character in savedCharIndexs
+        }
+
+        // If the word was typed correctly, increment the count
+        if (isWordCorrect) {
+            totalCorrectWords++;
+        }
+
+        // Move past the space character to the next word
+        currentIndex++; // Move past the space (if not the last word)
+    }
+
+    // Debugging output
+    console.log(`Total Words: ${words.length}, Total Correct Words: ${totalCorrectWords}`);
+
+    // Return the total number of correct words
+    return totalCorrectWords;
 }
 
 function CalculateWPM()
 {
-
+    // Get words.
     let totalWordsGot = GetCorrectWords();
+    if (mode == "TimeTest")
+    {
+        totalWordsGot = GetWordsTyped();
+        currentQuote = currentQuote.substring(0, currentCharIndex);
+        console.log(currentQuote);
+    }
 
     // Calculate time taken.
     let totalTime = timeEnd - timeStart;
+
     let totalMinutes = totalTime / 60;
-    
+    console.log("Words Correct: " +totalWordsGot);
+
     // Calculate wpm.
     let WPM = totalWordsGot / totalMinutes;
     
@@ -177,36 +364,28 @@ function CalculateWPM()
 
 function UpdateParagraph(currentString)
 {
-    // Reset text.
-    paragraphText.innerHTML = "";
+    // Loop through each index and check if it's correct.
+    const updatedHTML = currentString.split('').map((char, index) => {
+        // Check if it's right or wrong.
+        let isCorrect = savedCharIndexs[index] ? 'correctCharacter' : savedCharIndexs[index] == null || savedCharIndexs[index] == undefined ? "" : 'incorrectCharacter';
+        let extraID = "";
+
+        // Then check if it's the current item.
+        if (currentCharIndex == index)
+        {
+            extraID = "currentLetter";
+        }
+
+        // Create span.
+        return `<span class="${isCorrect}" id="${extraID}">${char}</span>`;
+    }).join('');
+
+    // Set the innerHTML of the paragraph text
+    paragraphText.innerHTML = updatedHTML;
+
+    // Scroll to element.
+    if ( document.getElementById("currentLetter"))  document.getElementById("currentLetter").scrollIntoView();
     
-     // Loop through and set colors.
-    for (let i = 0; i < currentString.length; i++)
-    {
-        // Create variable.
-        let spanType;
-            
-        // Get correct span type.
-        if (i < currentCharIndex)
-        {
-            // Check if the key pressed was correct.
-            if (savedCharIndexs[i] == true)
-            {
-                spanType = "<span class='correctCharacter'>";  
-            }
-            else if (savedCharIndexs[i] == null || savedCharIndexs[i] == undefined || savedCharIndexs[i] == false)
-            {
-                spanType = "<span class='incorrectCharacter'>";  
-            }
-        }
-        else
-        {
-            spanType = "";
-        }
-            
-        // Set span.
-        paragraphText.innerHTML += spanType + currentString[i] + "</span>";
-    }
 }
 
 function Keybinds(event)
@@ -223,7 +402,7 @@ function Keybinds(event)
 
 function KeyPressed(event)
 {
-    if (!isTyping) return;
+    if (!isTyping || menuOpen) return;
     
     // Get key we pressed.
     let keyCode = event.key;
@@ -248,6 +427,9 @@ function KeyPressed(event)
                 misses--;
             }
         }
+
+        // Remove from list
+        savedCharIndexs[currentCharIndex] = undefined;
         
         // Reset the text.
         UpdateParagraph(paragraphText.innerText);
@@ -255,9 +437,9 @@ function KeyPressed(event)
         // Also reset the text.
         return;
     }
-    
-    
-    
+
+   
+
     
     // Get the key we need to press next.
     let currentText = paragraphText.innerText[currentCharIndex];
@@ -286,6 +468,44 @@ function KeyPressed(event)
         
     // Update the paragraph.
     UpdateParagraph(currentString);
+
+     // Start the test
+     if (hasStartedTimer == false && mode == "TimeTest")
+        {
+            // Star the time test.
+            console.log("starting time test of " + timeTestLength);
+            hasStartedTimer = true;
+            currentTimeTest = new Date().getTime() / 1000;
+    
+            // Set interval
+            let timePassed = 0;
+            timerInterval = setInterval(() => {
+                // Increase variable as one.
+                timePassed++;
+                timeLeft.text(timeTestLength - timePassed + " s");
+    
+                // Wait for test to end.
+                if (timePassed >= timeTestLength)
+                {
+                    // End the test.
+                    isTyping = false;
+                    hasStartedTimer = false;
+                    console.log("time test end");
+    
+                    // Clear interval and show menu.
+                    clearInterval(timerInterval);
+                    currentTimeTest = 0;
+                    timerInterval = null;
+
+                    // Reset data.
+                    stillShowData = true;
+                    timeStart = 0;
+                    timeEnd = timeTestLength;
+                    timeLeft.fadeOut();
+                    LoadMenu(true);
+                }
+            }, 1000);    
+        }
     
     // Check if we finished
     if (currentCharIndex >= currentString.length)
@@ -301,12 +521,103 @@ function KeyPressed(event)
     }
 }
 
+function OpenSettings()
+{
+    // Check if is typing.
+    if (isTyping) return;
+
+    // Reset menu.
+    menuOpen = !menuOpen;
+
+    // Show the menu.
+    if (menuOpen) $("#settingsMenu").fadeIn();
+    else $("#settingsMenu").fadeOut();
+}
+
 // CONNECTIONS
 window.addEventListener('keydown', KeyPressed);
 window.addEventListener('keydown', Keybinds);
 homeButton.addEventListener('click', () => {
     LoadMenu(false);
 });
+settingsButton.addEventListener("click", OpenSettings);
+document.getElementById("RandomQuote").addEventListener("click", () => {
+  
+
+            userData.testType = "RandomQuotes";
+    $("#settingsMenu").fadeOut();
+});
+document.getElementById("RandomWords").addEventListener("click", () => {
+    let words = prompt("How many words would you like?");
+
+    // Check if word is good.
+    if (words.length > 0 && words != null && words != undefined)
+    {
+        let numberForm = parseInt(words);
+        if (numberForm > 0 && numberForm != null && numberForm != undefined)
+        {
+            // Set total words correctly.
+            $("#settingsMenu").fadeOut();
+             userData.testWords = Math.round(numberForm);
+            userData.testType = "RandomWords";
+            return;
+        }
+        else
+        {
+            // Alert user
+            alert("Please enter a number above 0.");
+            return;
+        }
+    }
+
+    // Alert user
+    alert("Please enter a real number.");
+});
+document.getElementById("TimeTest15").addEventListener("click", () => {
+    // Set mode and cloes
+    userData.testSeconds = 15;
+    userData.testType = "TimeTest";
+    $("#settingsMenu").fadeOut();
+})
+
+document.getElementById("TimeTestCustom").addEventListener("click", () => {
+    let words = prompt("How many seconds?");
+
+    // Check if word is good.
+    if (words.length > 0 && words != null && words != undefined)
+    {
+        let numberForm = parseInt(words);
+        if (numberForm > 0 && numberForm != null && numberForm != undefined)
+        {
+            // Set total words correctly.
+            $("#settingsMenu").fadeOut();
+            userData.testSeconds = Math.round(numberForm);
+            userData.testType = "TimeTest";
+        
+           
+            console.log(timeTestLength);
+            return;
+        }
+        else
+        {
+            // Alert user
+            alert("Please enter a number above 0.");
+            return;
+        }
+    }
+
+    // Alert user
+    alert("Please enter a real number.");
+});
+document.getElementById("ResetData").addEventListener("click", () => {
+    localStorage.setItem(DATA_KEY, JSON.stringify(gameData));
+    userData = gameData;
+    alert("Successfully reset data.");
+    $("#settingsMenu").fadeOut();
+});
+window.onbeforeunload = function() {
+    localStorage.setItem(DATA_KEY, JSON.stringify(userData));
+  };
 
 // INITIALIZATION
 Start();
